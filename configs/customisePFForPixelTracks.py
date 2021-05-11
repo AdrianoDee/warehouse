d9a8652befc7c63dc7399c13b1e9799df65af2ce
AdrianoDee <adriano.diflorio@ba.infn.it>
2021-05-11 17:33:45 +0200

    Fix

--- configs/customisePFForPixelTracks.py
+++ configs/customisePFForPixelTracks.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-def customisePFForPixelTracks():
+def customisePFForPixelTracks(process):
 
     process.hltPFMuonMerging.TrackProducers = cms.VInputTag("hltIterL3MuonTracks", "hltPixelTracksClean")
     process.hltPFMuonMerging.selectedTrackQuals = cms.VInputTag("hltIterL3MuonTracks", "hltPixelTracksClean")
