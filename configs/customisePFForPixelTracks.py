c95ededaeac976554e8c3b2596ea51ec51728f93
AdrianoDee <AdrianoDee@users.noreply.github.com>
2021-05-31 11:16:48 +0200

    Removing track quality cut from PFBlock

--- configs/customisePFForPixelTracks.py
+++ configs/customisePFForPixelTracks.py
@@ -4,5 +4,7 @@ def customisePFForPixelTracks(process):
 
     process.hltPFMuonMerging.TrackProducers = cms.VInputTag("hltIterL3MuonTracks", "hltPixelTracksClean")
     process.hltPFMuonMerging.selectedTrackQuals = cms.VInputTag("hltIterL3MuonTracks", "hltPixelTracksClean")
+    
+    process.hltParticleFlowBlock.trackQuality = cms.string('any')
 
     return process
